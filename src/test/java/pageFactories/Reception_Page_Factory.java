bb0e3c32b04ab4ec45462f14b5150a591eb21ca3
Jamie Shannon <shannon.jamie@gmail.com>
2017-06-25 09:39:21 +0100

    1. Added Reception_Page_Factory template.

---
+++ src/test/java/pageFactories/Reception_Page_Factory.java
@@ -0,0 +1,13 @@
+package pageFactories;
+
+/**
+ * Created by shann on 25/06/2017.
+ *
+ */
+public class Reception_Page_Factory {
+
+    public void placeholder() {
+        System.out.println("placeholder.");
+    }
+
+}
